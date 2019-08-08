16755b24fbfee4f3c632053a9d15e6ca26e22625
Jakub Szczerbiński <vszczerbinski@gmail.com>
2019-08-08 11:18:16 +0200

    work in progress

--- app/src/schedule/urls.py
+++ app/src/schedule/urls.py
@@ -4,9 +4,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^events/(?P<version>(v1))/(?P<pk>\d+)', views.EventDetail.as_view(), name="event-detail"),
     url(r'^$', views.index, name='schedule'),
     url(r'^add_event$', views.add_event, name='add_event'),
     url(r'^event/(?P<event_id>\d+)/edit$', views.edit_event, name='event edit'),
     url(r'^event/(?P<event_id>\d+)/show$', views.show_event, name='event_show'),
-    url(r'^events$', views.events, name='events')
+    url(r'^events$', views.events, name='events'),
+    url(r'^planner', views.planner, name='planner'),
 ]
--- app/src/schedule/views.py
+++ app/src/schedule/views.py
@@ -8,6 +8,21 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.translation import ugettext_lazy as _
 from django.http.response import Http404
 
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import EventSerializer
+
+
+class EventDetail(APIView):
+    def get(self, request, version, pk, format=None):
+        event = get_object_or_404(Event, pk)
+        serializer = EventSerializer(event, context={'request': request})
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 @login_required
 @require_http_methods(['GET'])
 def index(request):
@@ -66,3 +81,6 @@ def show_event(request, event_id=None):
     event = get_object_or_404(Event, pk=event_id)
     ctx = {'event': event}
     return render(request, 'schedule/show_event.html', ctx)
+
+def planner(request):
+    return render(request, 'schedule/planner.html')
